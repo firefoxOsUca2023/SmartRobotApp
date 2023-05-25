d5ffc3a67f8ec6dc07865df30736d7f56201008b
00218621 <00218621@uca.edu.sv>
2023-05-24 21:05:48 -0600

    nav_graph in navigation Main/Bluetooth

--- Proyecto_PDM/app/src/main/java/com/example/zacatales/smartrobotapp/MainActivity.kt
+++ Proyecto_PDM/app/src/main/java/com/example/zacatales/smartrobotapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.zacatales.smartrobotapp
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -12,10 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
     }
+
+
 
 }
